f9a4c50c9cb2128f7206669bf13eff2f06bdcfeb
WittCode <wittcode@gmail.com>
2023-03-12 16:33:24 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
+app.listen(1234);
